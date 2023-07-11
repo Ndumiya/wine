b73d2a3c7b806d5dd5075673fdbcc872d0b494fa
Ndumiya <Ndumiya@gmail.com>
2023-07-11 10:52:47 +0200

    done with About and source code

--- src/components/AboutBody.jsx
+++ src/components/AboutBody.jsx
@@ -21,14 +21,18 @@ function AboutBody() {
     </div>
 
     <div class="wrapper">
-      <h2>About</h2>
+      <h2>About us</h2>
       <div>
         <img src="https://vidyasheela.com/web-contents/website-components/About-Us-Pages/responsive-about-us-page-html/teaching.png" alt="img"/>
-        <p>A school is an educational institution designed to provide learning spaces and learning environments for the teaching of students under the direction of teachers. Most countries have systems of formal education, which is sometimes compulsory.
-          teenagers who have completed primary education. An institution where higher education is taught, is commonly called a university college or university. </p>
-        <p>A school is an educational institution designed to provide learning spaces and learning environments for the teaching of students under the direction of teachers. Most countries have systems of formal education, which is sometimes compulsory.
-          teenagers who have completed primary education. An institution where higher education is taught, is commonly called a university college or university. </p>
+        
+        <h4>What is the introduction of wine?</h4>
+        <p>wine, Alcoholic beverage made from the fermented juice of grapes. Wine may also be made from various fruits and plant parts. Though known by the ancients, wine was not drunk in its matured form until the development of the bottle and cork in the late 17th century. </p>
 
+        
+        <h4>Your Wine Has a Story</h4>
+        <p>Knowing how various cultures have produced and used wine over the years can enhance your wine drinking experience. From the first winery hidden in Armenia to the Phoenicians’ worldwide influence of winemaking to the social and spiritual aspects of wine in Greco-Roman culture and beyond, wine is much more than just fermented grapes in a glass. It's a lesson in history that you can savor every time you take a sip.</p>
+        
+        
       </div>
     </div>
 
@@ -36,22 +40,18 @@ function AboutBody() {
       <h2>Faculties</h2>
       <div class="faculties">
         <div class="unit">
-          <img src="https://vidyasheela.com/web-contents/website-components/About-Us-Pages/responsive-about-us-page-html/Director.jpg" alt=""/>
-          <p>Jona Chen, Director</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus unde aliquid laborum voluptatum distinctio nobis?'</p>
+        <h4>Wine bottles wrapped in short stories let you read while you drink</h4>
+          <img src="https://hips.hearstapps.com/rover/profile_photos/d2c7769a-7261-4da7-9296-a564ec697b91_1488471709.tmp3pbs9o71?fill=1:1&resize=200:*" alt=""/>
+          <p>BY KATIE FROSTPUBLISHED: 16 NOVEMBER 2016</p>
+          <p>A glass of wine and a good book is the ultimate treat on a quiet evening in and thanks to one Italian wine company, there's a brand new way to enjoy this great pairing.
+
+The Matteo Correggia winery has teamed up with product design agency Reverse Innovation to create a range of wines called Librottiglia.
+
+Each bottle, which is half the size of a full bottle, has been wrapped in a short story to complement the style of the wine. And there's even a clever reason behind the smaller bottles - they contain around two glasses, which is the perfect amount to get you through each "label book."'</p>
 
         </div>
-        <div class="unit">
-          <img src="https://vidyasheela.com/web-contents/website-components/About-Us-Pages/responsive-about-us-page-html/Principal.jpg" alt=""/>
-          <p>Mathew Tram, Principal</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus unde aliquid laborum voluptatum distinctio nobis?'</p>
-
-        </div>
-        <div class="unit">
-          <img src="https://vidyasheela.com/web-contents/website-components/About-Us-Pages/responsive-about-us-page-html/vice-principal.jpg" alt=""/>
-          <p>Lawn Sethi, Vice Principal</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus unde aliquid laborum voluptatum distinctio nobis?'</p>
-        </div>
+        
+        
       </div>
     </div>
 
